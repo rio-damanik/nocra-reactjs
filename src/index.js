43494a104a40fb5d878879be197a02ab356a4efa
rio-damanik <rio71385@gmail.com>
2024-07-26 21:10:38 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div>Hello World</div>
+        <div>Ini Adalah Update REACT WITH NO CRA</div>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
